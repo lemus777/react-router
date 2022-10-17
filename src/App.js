46cbff6302316be72d35582edcd590f371f80edf
lemus <lemus777@gmail.com>
2022-10-17 22:34:49 +0200

    creación de las rutas básicas

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Menu } from './Menu';
+import { HomePage } from './HomePage';
+import { BlogPage } from './BlogPage';
+import { ProfilePage } from './ProfilePage';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/blog' element={<BlogPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='*' element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+// Al usar HashRouter la dirección principal sería /#/
+// El resto sería usando el hash como raiz: /#/blog
+// El * es el que se usa por defecto con cualquier otra dirección no listada en las routes
